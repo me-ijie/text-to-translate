862ce191b3cf4fe30f945807d7009e3164645e2a
梅婕 <draltracy@msn.cn>
2023-03-13 18:41:53 +0800

    update(功能代码): 扩展右键菜单并读取文件内容

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,21 +13,100 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "text-to-translate" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('text-to-translate.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = []
+  let item = vscode.commands.registerCommand('text-to-translate.helloWorld', function () {
+    vscode.window.showInformationMessage('Hello World from text-to-translate!')
+  })
+  disposable.push(item)
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from text-to-translate!');
-	});
+  item = vscode.commands.registerCommand("text-to-translate.doTranslation", async (...commandArgs) => {
+  
+    const modifyFiles = (resources) => {
+      for (let resource of resources) {
+        let filePath = resource.fsPath || resource
+        let stats = fs.statSync(filePath);
+        if (stats.isFile()) {
+          try {
+            let fileContent = fs.readFileSync(filePath, 'utf8');
+            let content = fileContent.replace(/(^\s*[a-zA-Z-]+=")([\u4e00-\u9fa5]+)"$/gi, ($1, $2) => {
+              console.log('$2', $2)
+              return ':$1$t(\'$2\')"'
+            })
+            console.log('content', content)
+            fs.writeFileSync(filePath, content);
+            // const relativePath = vscode.workspace.asRelativePath(resource)
+            // const thisResource = vscode.Uri.parse(resource._formatted)
+            // 打开文件
+            const openPath = vscode.Uri.file(resource);
+            vscode.workspace.openTextDocument(openPath).then(doc => {
+              vscode.window.showTextDocument(doc);
+            });
+          } catch (e) {
+            vscode.window.showInformationMessage('You do not have permission to access this file!')
+          }
 
-	context.subscriptions.push(disposable);
+        } else if (stats.isDirectory()) {
+          fs.readdir(filePath, (err, files) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            let newResources = files.map(file => {
+              const newPath = path.join(filePath, file);
+              return newPath
+            });
+            modifyFiles(newResources)
+          });
+        }
+      }
+    }
+    if (commandArgs[1][0] instanceof vscode.Uri)  modifyFiles(commandArgs[1])
+    vscode.window.showInformationMessage('translation completed!')
+  })
+  disposable.push(item)
+
+    // vscode.commands.registerCommand('text-to-translate.addshortcuts', function () {
+
+    
+    // let shortcuts = [
+    //   {
+    //     "key": "ctrl+r",
+    //     "command": "editor.action.insertSnippet",
+    //     "args": { "snippet": "{{\\$t('${TM_SELECTED_TEXT}$1')}}" },
+    //     "when": "editorTextFocus&&editorHasSelection",
+    //     "name": "wrapText"
+    //   },
+    //   {
+    //     "key": "ctrl+e",
+    //     "command": "editor.action.insertSnippet",
+    //     "args": { "snippet": "$1\\$t(${TM_SELECTED_TEXT})" },
+    //     "when": "editorTextFocus&&editorHasSelection",
+    //     "name": "wrapText2"
+    //   },
+    //   {
+    //     "key": "ctrl+w",
+    //     "command": "editor.action.insertSnippet",
+    //     "args": { "snippet": "${TM_SELECTED_TEXT/([a-zA-Z-]+=\")([^\"]+)\"/:$1$t('$2')\"/g}" },
+    //     "when": "editorTextFocus&&editorHasSelection",
+    //     "name": "wrapText3"
+    //   },
+    //   {
+    //     "key" : "ctrl+shift+u",
+    //     "command" : "editor.action.transformToUppercase",
+    //     "when" : "editorHasSelection",
+    //     "name": "wrapText4"
+    //   }
+    // ]
+    // for (let keyObj of shortcuts) {
+    //   let name = keyObj['name']
+    //   delete keyObj.name
+    //   vscode.commands.executeCommand(`text-to-translate.${name}`, keyObj)
+    // })
+
+
+	context.subscriptions.push(...disposable);
 }
 
 // This method is called when your extension is deactivated
