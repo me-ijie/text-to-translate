55a3ac91a1429992ac6645d78a521c46549f5313
梅婕 <draltracy@msn.cn>
2023-03-20 23:54:15 +0800

    update(features): fix async function call

--- extension.js
+++ extension.js
@@ -23,6 +23,18 @@ function activate(context) {
 
   item = vscode.commands.registerCommand("text-to-translate.doTranslation", async (...commandArgs) => {
 
+    // 写入待翻译文本文档
+    const storeForTranslation = async (content) => {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+    
+      if (!workspaceFolders) return vscode.window.showErrorMessage('No workspace is open');
+      
+      const allFiles = await vscode.workspace.findFiles('src/i18n/**/*', '**/node_modules/**');
+      const targetFile = allFiles.find(file => path.basename(file.fsPath) === 'i18n-translate-template.txt');
+      if (!targetFile) return vscode.window.showErrorMessage('File i18n-translate-template not found in the workspace');
+
+      fs.writeFileSync(targetFile.fsPath, content);
+    }
     // 用于保存 待翻译的文本
     let holdSpace = []
     // 遍历当前文件/文件夹下的文件
@@ -36,7 +48,6 @@ function activate(context) {
           try {
             let fileContent = fs.readFileSync(filePath, 'utf8');
             let content = editContent(fileContent, holdSpace)
-            console.log('holdSpace', holdSpace)
             fs.writeFileSync(filePath, content);
 
             // 打开文件
@@ -49,49 +60,19 @@ function activate(context) {
           }
 
         } else if (stats.isDirectory()) {
-          fs.readdir(filePath, (err, files) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-            let newResources = files.map(file => {
-              const newPath = path.join(filePath, file);
-              return newPath
-            });
-            modifyFiles(newResources)
+          let files = fs.readdirSync(filePath)
+          let newResources = files.map(file => {
+            const newPath = path.join(filePath, file);
+            return newPath
           });
+          modifyFiles(newResources)
         }
       }
 
-      return holdSpace
+      if (resources === commandArgs[1] && holdSpace.length) storeForTranslation(holdSpace.join('\n'))
     }
 
-    // 写入待翻译文本文档
-    const storeForTranslation = async (content) => {
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-    
-      if (!workspaceFolders) {
-        vscode.window.showErrorMessage('No workspace is open');
-        return;
-      }
-      
-      const allFiles = await vscode.workspace.findFiles('src/i18n/**/*', '**/node_modules/**');
-      const targetFile = allFiles.find(file => path.basename(file.fsPath) === 'i18n-translate-template.txt');
-      if (!targetFile) {
-        console.log('allfiles', allFiles)
-        vscode.window.showErrorMessage('File i18n-translate-template not found in the workspace');
-        return;
-      }
-
-      fs.writeFileSync(targetFile.fsPath, content);
-    }
-    if (commandArgs[1][0] instanceof vscode.Uri) {
-      modifyFiles(commandArgs[1])
-    }
-    console.log('holdspace after', holdSpace)
-    if (holdSpace.length) {
-      storeForTranslation(holdSpace.join('\n'))
-    }
+    if (commandArgs[1][0] instanceof vscode.Uri) modifyFiles(commandArgs[1])
 
     vscode.window.showInformationMessage('translation completed!')
   })
